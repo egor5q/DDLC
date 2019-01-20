a08a77c9b5eeecd9099bdf41de6d74589dbc4a7b
egor5q <31898567+egor5q@users.noreply.github.com>
2019-01-20 21:19:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,16 +8,46 @@ from emoji import emojize
 from telebot import types
 from pymongo import MongoClient
 
-
-token = os.environ['TELEGRAM_TOKEN']
-bot = telebot.TeleBot(token)
+monika = telebot.TeleBot(os.environ['monika'])
+sayori = telebot.TeleBot(os.environ['sayori'])
+yuri = telebot.TeleBot(os.environ['yuri'])
+natsuki = telebot.TeleBot(os.environ['natsuki'])
 
 
 client=MongoClient(os.environ['database'])
-db=client.
+db=client.ddlc
 users=db.users
 
+monika_hello_basic=['Здравствуй, наш дорогой гость! Тут ты сможешь пообщаться с уже вступившими членами клуба - Всегда'+\
+                    'весёлой Сайори, начитанной Юри и миленькой Нацуки.']
+#monika_hello_basic=['Добро пожаловать в литературный клуб! Я всегда мечтала создать уютное место, где можно хорошо провести время. '+\
+#              'Здесь мы можем совместно улучшать свои навыки в чтении литературы и не только.','Привет, это литературный клуб. '+\
+#              'Рада тебя видеть здесь, нам сейчас очень нужны новые члены, и ты очень кстати.','Привет! Хорошо, что ты заглянул '+\
+#                    '- тут мы улучшаем свои навыки в прочтении литературы, и просто проводим время вместе! Надеюсь, что ты останешься '+\
+#                    'с нами надолго.','Здравствуй, наш дорогой гость! Тут ты сможешь пообщаться с уже вступившими членами клуба - Всегда'+\
+#                    'весёлой Сайори, начитанной Юри и миленькой Нацуки.','Приветствую тебя в Литературном клубе. Проходи не стесняйся,'+\
+#                    'Нацуки уже испекла кексики к твоему приходу, а Юри сделает чай, если ты хочешь.']
 
+@bot.message_handler(content_types=['new_chat_members'])
+def newchatmember(m):
+   if users.find_one({'id':m.from_user.id})==None:
+      hello=random.choice(monika_hello_basic)
+      sendact(m.chat.id, monika, 'typing')
+      t=threading.Timer(4,sendm,args=[m.chat.id,monika,hello])
+      t.start()
+      if 'миленькой Нацуки' in hello:
+         t=threading.Timer(5,sendact,args=[m.chat.id,natsuki,'typing'])
+         t.start()
+         t=threading.Timer(7,sendm,args=[m.chat.id,natsuki,'Я не миленькая!!!!'])
+         t.start()
+      
+
+def sendact(id,bot,act):
+   bot.send_chat_action(id,act)
+      
+def sendm(id,bot,text,parse_mode=None):
+   bot.send_message(id,text,parse_mode=parse_mode)
+      
 
 if True:
    print('7777')
