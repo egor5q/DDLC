5bf49f4924a28255e9852532f5fcb47651e08ae5
egor5q <31898567+egor5q@users.noreply.github.com>
2019-01-21 02:15:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telebot import types
 from pymongo import MongoClient
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-
+from nltk.stem import PorterStemmer
 
 monika = telebot.TeleBot(os.environ['monika'])
 sayori = telebot.TeleBot(os.environ['sayori'])
